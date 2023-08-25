29a9ec7421fd2836a948908acbbc22837fb8af32
Reem-Nosbaum <reemnu@gmail.com>
2023-08-25 12:36:21 +0300

    Improve structure / format of the code. :art:

--- src/comp/NavBar.jsx
+++ src/comp/NavBar.jsx
@@ -1,6 +1,20 @@
 import "../style/App.css";
 import "../style/NavBar.css";
+import { AiOutlineHome, AiOutlineMenu } from "react-icons/ai";
 
 export default function NavBar() {
-  return <div className="nav-bar">hello</div>;
+  function hendalHomeButton(e) {
+    console.log("press");
+  }
+  return (
+    <div className="nav-bar">
+      - Roolet -
+      <button className="home-btn" onClick={hendalHomeButton}>
+        <AiOutlineHome size={"25px"} />
+      </button>
+      <button className="menu-btn">
+        <AiOutlineMenu size={"25px"} />
+      </button>
+    </div>
+  );
 }
